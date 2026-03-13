c8026cce8c53279e8789678a2ec8408112ad7d4a
Ben McNamee <khtiniop@gmail.com>
2026-03-13 03:55:23 -0500

    716
    reached 716

--- Assets/FollowCam.cs
+++ Assets/FollowCam.cs
@@ -6,6 +6,10 @@ public class FollowCam : MonoBehaviour
 {
     static public GameObject POI;
 
+    [Header("Inscribed")]
+    public float easing =0.05f;
+    public Vector2 minXY =Vector2.zero;
+
     [Header("Dynamic")]
     public float camZ;
 
@@ -16,10 +20,31 @@ public class FollowCam : MonoBehaviour
     
     void FixedUpdate()
     {
-        if(POI ==null ) return;
+        // if(POI ==null ) return;
 
-        Vector3 destination = POI.transform.position;
+        // Vector3 destination = POI.transform.position;
+        Vector3 destination = Vector3.zero;
+        if (POI != null)
+        {
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if((poiRigid != null)&& poiRigid.IsSleeping())
+            {
+                POI=null;
+            }
+        }
+
+        if(POI != null)
+        {
+            destination = POI.transform.position;
+        }
+
+
+        destination.x= Mathf.Max(minXY.x, destination.x);
+        destination.y= Mathf.Max(minXY.y, destination.y);
+        destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
      }
 }
