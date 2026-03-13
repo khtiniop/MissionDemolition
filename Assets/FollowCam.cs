ae166bff930705fcd7337ab617efef44aea7b0a5
Ben McNamee <khtiniop@gmail.com>
2026-03-13 03:06:05 -0500

    Page 705
    currently on 705

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+
+    [Header("Dynamic")]
+    public float camZ;
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+    
+    void FixedUpdate()
+    {
+        if(POI ==null ) return;
+
+        Vector3 destination = POI.transform.position;
+        destination.z = camZ;
+        transform.position = destination;
+     }
+}
